59912d2e7e7336fa27242d3eb6c362a80fb9fecf
John Shamoon <johnshamoon96@gmail.com>
2020-12-13 23:04:13 -0800

    Add !summoner command
    Adds the !summoner command to get the summoner's level.

--- src/main.py
+++ src/main.py
@@ -5,11 +5,12 @@ from dotenv import load_dotenv
 from rift_harold_client import RiftHaroldClient
 
 load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
+DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
+RIOT_TOKEN = os.getenv('RIOT_TOKEN')
 
 def main():
-  client = RiftHaroldClient()
-  client.run(TOKEN)
+  client = RiftHaroldClient(RIOT_TOKEN)
+  client.run(DISCORD_TOKEN)
 
 if __name__ == '__main__':
   main()
--- src/rift_harold_client.py
+++ src/rift_harold_client.py
@@ -1,10 +1,22 @@
 import os
+import requests
 
 import discord
 
 class RiftHaroldClient(discord.Client):
   """Implements event handlers from discord.Client.
   """
+  def __init__(self, riot_api_key):
+    discord.Client.__init__(self)
+    self.riot_api_key = riot_api_key
+
+  def get_summoner_info(self, summoner_name):
+    url = 'https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name'
+    request =  '{}/{}?api_key={}'.format(url, summoner_name, self.riot_api_key)
+    response = requests.get(request)
+
+    return response.json()
+
   async def on_ready(self):
     print('Logged on as {0}!'.format(self.user))
 
@@ -12,6 +24,11 @@ class RiftHaroldClient(discord.Client):
     # Skip the message if it came from RiftHarold
     if message.author == self.user:
       return
-    
-    if message.content.startswith('!hello'):
-      await message.channel.send('hello')
+
+    if message.content.startswith('!summoner'):
+      requested_summoner_name = message.content.split()[1]
+      summoner_info = self.get_summoner_info(requested_summoner_name)
+      response = 'Summoner Name: {}\nSummoner Level: {}\n'.format(summoner_info['name'], summoner_info['summonerLevel'])
+      await message.channel.send(response)
+    elif message.content.startswith('!'):
+      await message.channel.send("Supported commands: !summoner")
