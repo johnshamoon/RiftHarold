35bfd7c2e63a307e7b3aba752243b903f648fab2
jnotor <j.notorgiacomo@gmail.com>
2020-12-18 16:02:01 -0500

    cleaned up the start tracking function and added some set up functions. finalized get live data

--- src/rift_harold.py
+++ src/rift_harold.py
@@ -6,25 +6,14 @@ class RiftHarold:
   _base_url = 'https://na1.api.riotgames.com/lol/'
 
   def  __init__(self, riot_api_key):
+    self.game = {}
+    self.champs = {}
+    self.runes = {}
+    self.summoner_spells = {}
+
     self.riot_api_key = riot_api_key
 
-    # Set up information for SS calc. based on runes
     # TODO: maybe add region detection future
-    with open('../data/current/data/en_US/runesReforged.json') as f:
-      rune_data = f.read()
-
-    # Initial data format is a list of dicts; load Runes into more useful format
-    rune_list = json.loads(rune_data)
-
-    self.runes = {}
-    # rune_tree refers to the 6 different rune paths overall
-    for rune_tree in rune_list:
-      # rune_row refers to each row of choices in a tree
-      for rune_row in rune_tree['slots']:
-        # rune_row contains list of choice dicts
-        for rune_choice in rune_row['runes']:
-          self.runes[rune_choice['id']] = { 'name' : rune_choice['key'],
-                                       'desc' : rune_choice['longDesc']}
 
 
   def get_summoner_info(self, summoner_name, internalUse=False):
@@ -41,7 +30,6 @@ class RiftHarold:
     # I need for more than a print out; need encryptedId and maybe more
     if internalUse:
       return response
-    print(response)
     info = 'Summoner Name: {}\nSummoner Level: {}\n'.format(response['name'], response['summonerLevel'])
 
     return info
@@ -53,25 +41,70 @@ class RiftHarold:
     response = requests.get(request)
 
     if response.status_code == 200:
-      returnString = 'has '
-      # TODO: parse response with more than one person in game and match perks to player
-      current_runes = re.findall('(?<=perkIds":\[).*?(?=\],")', response.text)
-      # NOTE: current_runes will likely be a list of 10 elements outside of testing : ^ )
-      if current_runes:
-        # NOTE: obvi this wont work for more than one person
-        current_runes = current_runes[0].split(',')
-        for rune in current_runes:
-          # FIXME: keyerror on 5008??? is the adaptive and armor stuff but thats not in our dict. hm
-          if rune == '5008':
-            break
-          returnString += self.runes[int(rune)]['name'] + ' '
+      response_text = json.loads(response.text)
+      for player in response_text['participants']:
+        # remove the bottom 3 "perks" that are not actually runes
+        perks = player['perks']['perkIds'][:-3]
 
-      return returnString
+        self.game[player['championId']] = { 'team' : player['teamId'],
+                                            'summoner_spell_1' : player['spell1Id'],
+                                            'summoner_spell_2' : player['spell2Id'],
+                                            'runes' : perks}
+      return 'Started tracking game'
     else:
       return 'Cannot get live data at this time. Response code:%s' % response.status_code
 
 
+  def setup_champs(self):
+    with open('../data/current/data/en_US/championsFull.json') as f:
+      data = f.read()
+
+    champ_dict = json.loads(data)
+    champ_dict = champ_dict['data']
+    for champ in champ_dict:
+      # Grab spells, description, cooldowns; want to index by champ key
+      self.champs[champ['key']] = { 'name' : champ['name'],
+                                    # NOTE: may want to do, per spell: id, name, cooldown, cost, description
+                                    'spells' : champ['spells'],
+                                    'passive' : champ['passive']}
+
+
+  def setup_runes(self):
+    # Set up information for SS calc. based on runes
+    with open('../data/current/data/en_US/runesReforged.json') as f:
+      rune_data = f.read()
+
+    # Initial data format is a list of dicts; load Runes into more useful format
+    rune_list = json.loads(rune_data)
+
+    # rune_tree refers to the 6 different rune paths overall
+    for rune_tree in rune_list:
+      # rune_row refers to each row of choices in a tree
+      for rune_row in rune_tree['slots']:
+        # rune_row contains list of choice dicts
+        for rune_choice in rune_row['runes']:
+          self.runes[rune_choice['id']] = { 'name' : rune_choice['key'],
+                                            'desc' : rune_choice['longDesc']}
+
+
+  def setup_summs(self):
+    # Get summoner spell info
+    with open('../data/current/data/en_US/summoner.json') as f:
+      summoner_spell_data = f.read()
+
+    ss_list = json.loads(summoner_spell_data)
+
+    for spell in ss_list:
+      # NOTE: There's also the key 'cooldown' but it has a list and i cant tell
+      # the difference between the two of them
+      self.summoner_spells[spell['key']] = spell['cooldownBurn']
+
+
   def start_tracking(self, summoner_name):
+    self.setup_champs()
+    self.setup_runes()
+    self.setup_summs()
+
     summonerData = self.get_summoner_info(summoner_name, True)
     return self.get_live_data(summonerData['id'])
 
